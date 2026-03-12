b982161df743513aed1f08ca1c7932cab093084d
jitv-dev <javier.torresvalenz@gmail.com>
2026-03-11 21:48:05 -0300

    fix linea 28 /productos/ a /productos

--- src/main/java/com/panaderia/rodrigo/config/SecurityConfig.java
+++ src/main/java/com/panaderia/rodrigo/config/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
                 )
                 .formLogin(form -> form
                         .loginPage("/login")
-                        .defaultSuccessUrl("/productos/", true)
+                        .defaultSuccessUrl("/productos", true)
                         .permitAll()
                 )
                 .logout(logout -> logout
