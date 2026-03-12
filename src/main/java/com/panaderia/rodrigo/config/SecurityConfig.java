e8bdd682c86d0a8e7075b1911f4ddf4254d77958
jitv-dev <javier.torresvalenz@gmail.com>
2026-03-11 21:01:43 -0300

    hasheo y usuarios iniciales

--- src/main/java/com/panaderia/rodrigo/config/SecurityConfig.java
+++ src/main/java/com/panaderia/rodrigo/config/SecurityConfig.java
@@ -1,4 +1,67 @@
 package com.panaderia.rodrigo.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/", "/productos", "/css/**", "/img/**").permitAll()
+                        .requestMatchers("/productos/nuevo", "/productos/").hasRole("ADMIN")
+                        .requestMatchers("/clientes/**").hasAnyRole("ADMIN", "EMPLEADO")
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/productos/", true)
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/login?logout")
+                        .permitAll()
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("admin123"))
+                .roles("ADMIN")
+                .build();
+
+        UserDetails empleado = User.builder()
+                .username("empleado")
+                .password(passwordEncoder().encode("emp123"))
+                .roles("EMPLEADO")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, empleado);
+    }
 }
