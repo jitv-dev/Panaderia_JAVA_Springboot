94975871e42a25ade70a50f279ed9469f0d8820a
jitv-dev <javier.torresvalenz@gmail.com>
2026-03-11 21:01:24 -0300

    datos iniciales

--- src/main/java/com/panaderia/rodrigo/loader/DataLoader.java
+++ src/main/java/com/panaderia/rodrigo/loader/DataLoader.java
@@ -1,4 +1,20 @@
 package com.panaderia.rodrigo.loader;
 
-public class DataLoader {
+import com.panaderia.rodrigo.model.Producto;
+import com.panaderia.rodrigo.repository.ProductoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+    @Autowired
+    private ProductoRepository productoRepository;
+
+    @Override
+    public void run(String... args) {
+        productoRepository.save(new Producto(null, "MediasLunas", "Facturas", 35.0, 5));
+        productoRepository.save(new Producto(null, "Marraqueta", "Pan", 120.0, 150));
+        productoRepository.save(new Producto(null, "Torta de Chocolate", "Tortas", 2800.0, 50));
+    }
 }
